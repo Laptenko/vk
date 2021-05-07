402b038db130ecbe1b0405e8b355c8f6da936d14
Maksim Laptenko <laptenko0811@icloud.com>
2021-05-07 16:41:03 +0300

    version 1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Post {
+    private String author;
+    private String authorImg;
+    private String date;
+    private String postText;
+    private String postImg;
+    private String postLikeImg;
+    private String postLikeText;
+    private String postLikeLink;
+    private String likeButtonIcon;
+    private int likeButtonCount;
+    private String commentButtonIcon;
+    private int commentButtonCount;
+    private String shareButtonIcon;
+    private int shareButtonCount;
+    private String viewIcon;
+    private int viewCount;
+
+
+}
